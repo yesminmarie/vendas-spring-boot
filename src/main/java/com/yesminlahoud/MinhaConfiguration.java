9cf65c1e568abd76edc5d922b682e34fa9b27363
yesmin <ymslahoud@gmail.com>
2021-01-23 12:57:14 -0300

    multiplos ambientes e profiles

--- src/main/java/com/yesminlahoud/MinhaConfiguration.java
+++ src/main/java/com/yesminlahoud/MinhaConfiguration.java
@@ -1,9 +1,18 @@
 package com.yesminlahoud;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguration {
 
+    @Bean
+    public CommandLineRunner executar() {
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
+    }
 }
