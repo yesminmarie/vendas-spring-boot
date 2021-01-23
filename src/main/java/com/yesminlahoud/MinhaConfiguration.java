74e9dd8e9ececb8addc77f259a012165c37458c2
yesmin <ymslahoud@gmail.com>
2021-01-23 11:02:45 -0300

    application properties

--- src/main/java/com/yesminlahoud/MinhaConfiguration.java
+++ src/main/java/com/yesminlahoud/MinhaConfiguration.java
@@ -6,8 +6,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MinhaConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName() {
-        return "Sistema de Vendas";
-    }
 }
