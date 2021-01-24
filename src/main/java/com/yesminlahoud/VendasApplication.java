18585cafc8ba318be3bf4cea965efe9d1c898f6f
yesmin <ymslahoud@gmail.com>
2021-01-24 14:54:54 -0300

    concluindo o cadastro de clientes

--- src/main/java/com/yesminlahoud/VendasApplication.java
+++ src/main/java/com/yesminlahoud/VendasApplication.java
@@ -16,11 +16,36 @@ public class VendasApplication {
     @Bean
     public CommandLineRunner init(@Autowired Clientes clientes){
         return args -> {
+            System.out.println("Salvando clientes");
             clientes.salvar(new Cliente( "Yesmin"));
-            clientes.salvar(new Cliente("Maria"));
+            clientes.salvar(new Cliente("Outro Cliente"));
 
             List<Cliente> todosClientes = clientes.obterTodos();
             todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando clientes");
+            todosClientes.forEach(c -> {
+                c.setNome((c.getNome() + " atualizado."));
+                clientes.atualizar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando clientes");
+            clientes.buscarPorNome("Cli").forEach(System.out::println);
+
+            //System.out.println("Deletando clientes");
+            //clientes.obterTodos().forEach(c -> {
+            //    clientes.deletar(c);
+            //});
+
+            todosClientes = clientes.obterTodos();
+            if(todosClientes.isEmpty()){
+                System.out.println("Nenhum cliente encontrado.");
+            }else{
+                todosClientes.forEach(System.out::println);
+            }
         };
     }
 
