494d20079b2975814bb98196c974130d65d322fb
yesmin <ymslahoud@gmail.com>
2021-01-24 14:01:01 -0300

    salvando e recuperando clientes

--- src/main/java/com/yesminlahoud/VendasApplication.java
+++ src/main/java/com/yesminlahoud/VendasApplication.java
@@ -1,10 +1,28 @@
 package com.yesminlahoud;
 
+import com.yesminlahoud.domain.entity.Cliente;
+import com.yesminlahoud.repositorio.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            clientes.salvar(new Cliente( "Yesmin"));
+            clientes.salvar(new Cliente("Maria"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
