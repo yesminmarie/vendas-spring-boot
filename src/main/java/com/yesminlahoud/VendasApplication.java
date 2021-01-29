7e38aa4f417851dab588ace03bf1d90f55179c5e
yesmin <ymslahoud@gmail.com>
2021-01-29 20:06:59 -0300

    query methods

--- src/main/java/com/yesminlahoud/VendasApplication.java
+++ src/main/java/com/yesminlahoud/VendasApplication.java
@@ -20,32 +20,35 @@ public class VendasApplication {
             clientes.save(new Cliente( "Yesmin"));
             clientes.save(new Cliente("Outro Cliente"));
 
-            List<Cliente> todosClientes = clientes.findAll();
-            todosClientes.forEach(System.out::println);
+            boolean existe = clientes.existsByNome("Ana");
+            System.out.println("existe um cliente com o nome Yesmin? " + existe);
 
-            System.out.println("Atualizando clientes");
-            todosClientes.forEach(c -> {
-                c.setNome((c.getNome() + " atualizado."));
-                clientes.save(c);
-            });
+//            List<Cliente> todosClientes = clientes.findAll();
+//            todosClientes.forEach(System.out::println);
 
-            todosClientes = clientes.findAll();
-            todosClientes.forEach(System.out::println);
-
-            System.out.println("Buscando clientes");
-            clientes.findByNomeLike("Cli").forEach(System.out::println);
-
-            System.out.println("Deletando clientes");
-            clientes.findAll().forEach(c -> {
-                clientes.delete(c);
-            });
-
-            todosClientes = clientes.findAll();
-            if(todosClientes.isEmpty()){
-                System.out.println("Nenhum cliente encontrado.");
-            }else{
-                todosClientes.forEach(System.out::println);
-            }
+//            System.out.println("Atualizando clientes");
+//            todosClientes.forEach(c -> {
+//                c.setNome((c.getNome() + " atualizado."));
+//                clientes.save(c);
+//            });
+//
+//            todosClientes = clientes.findAll();
+//            todosClientes.forEach(System.out::println);
+//
+//            System.out.println("Buscando clientes");
+//            clientes.findByNomeLike("Cli").forEach(System.out::println);
+//
+//            System.out.println("Deletando clientes");
+//            clientes.findAll().forEach(c -> {
+//                clientes.delete(c);
+//            });
+//
+//            todosClientes = clientes.findAll();
+//            if(todosClientes.isEmpty()){
+//                System.out.println("Nenhum cliente encontrado.");
+//            }else{
+//                todosClientes.forEach(System.out::println);
+//            }
         };
     }
 
--- src/main/java/com/yesminlahoud/repositorio/Clientes.java
+++ src/main/java/com/yesminlahoud/repositorio/Clientes.java
@@ -2,14 +2,12 @@ package com.yesminlahoud.repositorio;
 
 import com.yesminlahoud.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
-@Repository
 public interface Clientes extends JpaRepository<Cliente, Integer> {
 
-
     List<Cliente> findByNomeLike(String nome);
+
+    boolean existsByNome(String nome);
 }
