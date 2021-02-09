e63d7dd3248270303bb880ca40db3a72bbc02f40
yesmin <ymslahoud@gmail.com>
2021-02-09 20:34:29 -0300

    criando o controller de clientes

--- src/main/java/com/yesminlahoud/VendasApplication.java
+++ src/main/java/com/yesminlahoud/VendasApplication.java
@@ -1,45 +1,10 @@
 package com.yesminlahoud;
 
-import com.yesminlahoud.domain.entity.Cliente;
-import com.yesminlahoud.domain.entity.Pedido;
-import com.yesminlahoud.repository.Clientes;
-import com.yesminlahoud.repository.Pedidos;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
 public class VendasApplication {
-
-    @Bean
-    public CommandLineRunner init(
-            @Autowired Clientes clientes,
-            @Autowired Pedidos pedidos){
-        return args -> {
-            System.out.println("Salvando clientes");
-            Cliente fulano = new Cliente("Fulano");
-            clientes.save(fulano);
-
-            Pedido p = new Pedido();
-            p.setCliente(fulano);
-            p.setDataPedido(LocalDate.now());
-            p.setTotal(BigDecimal.valueOf(100));
-
-            pedidos.save(p);
-
-//            Cliente cliente = clientes.findClienteFetchPedido(fulano.getId());
-//            System.out.println(cliente);
-//            System.out.println(cliente.getPedidos());
-
-            pedidos.findByCliente(fulano).forEach(System.out::println);
-        };
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
