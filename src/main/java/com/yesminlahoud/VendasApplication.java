d1c2222856771bc47178e9d9a2b178f14445005b
yesmin <ymslahoud@gmail.com>
2021-02-13 16:36:59 -0300

    post delete put clientes

--- src/main/java/com/yesminlahoud/VendasApplication.java
+++ src/main/java/com/yesminlahoud/VendasApplication.java
@@ -11,14 +11,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class VendasApplication {
 
-    @Bean
-    public CommandLineRunner commandLineRunner(@Autowired ClientesRepository clientesRepository){
-        return args -> {
-            Cliente c = new Cliente(null, "Fulano");
-            clientesRepository.save(c);
-        };
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
--- src/main/java/com/yesminlahoud/rest/controller/ClienteController.java
+++ src/main/java/com/yesminlahoud/rest/controller/ClienteController.java
@@ -28,4 +28,37 @@ public class ClienteController {
 
         return ResponseEntity.notFound().build();
     }
+
+    @PostMapping("/api/clientes")
+    @ResponseBody
+    public ResponseEntity save( @RequestBody Cliente cliente){
+        Cliente clienteSalvo = clientesRepository.save(cliente);
+        return ResponseEntity.ok(clienteSalvo);
+    }
+
+    @DeleteMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity delete (@PathVariable Integer id){
+        Optional<Cliente> cliente= clientesRepository.findById(id);
+
+        if(cliente.isPresent()){
+            clientesRepository.delete(cliente.get());
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity update(@PathVariable Integer id,
+                                 @RequestBody Cliente cliente) {
+        return clientesRepository
+                .findById(id)
+                .map( clienteExistente -> {
+                    cliente.setId(clienteExistente.getId());
+                    clientesRepository.save(cliente);
+                    return ResponseEntity.noContent().build();
+                }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
