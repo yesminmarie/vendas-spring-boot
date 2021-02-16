57314d538ff2a674a68da71a9cd29ed624a8b7b3
yesmin <ymslahoud@gmail.com>
2021-02-16 14:14:56 -0300

    pesquisa de clientes por parametros

--- src/main/java/com/yesminlahoud/rest/controller/ClienteController.java
+++ src/main/java/com/yesminlahoud/rest/controller/ClienteController.java
@@ -2,10 +2,13 @@ package com.yesminlahoud.rest.controller;
 
 import com.yesminlahoud.domain.entity.Cliente;
 import com.yesminlahoud.repository.ClientesRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -60,5 +63,17 @@ public class ClienteController {
                     clientesRepository.save(cliente);
                     return ResponseEntity.noContent().build();
                 }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/api/clientes")
+    public ResponseEntity find(Cliente filtro) {
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(
+                        ExampleMatcher.StringMatcher.CONTAINING);
+        Example example = Example.of(filtro, matcher);
+        List<Cliente> lista = clientesRepository.findAll(example);
+        return ResponseEntity.ok(lista);
     }
 }
